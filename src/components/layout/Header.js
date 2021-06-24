618f49ff09ce9ef33ee2411e0df1b4d29729e434
Clément <contact@clement-menard.fr>
2021-06-24 10:06:01 +0200

    Init repo

---
+++ src/components/layout/Header.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+export class Header extends Component {
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                Hello world!
+            </nav>
+        );
+    }
+}
+export default Header
